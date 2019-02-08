deb8522d6213a6cb502d837a2c992cd00acb876a
oldwss <teodor.aspataritei@gmail.com>
2019-02-08 04:48:13 -0800

    added bid history

---
+++ models/Bid_history.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bidhSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // Status of bid : 1 = open bid   0 = closed bid
+  status: {
+    type: Boolean,
+    default: 1
+  }
+});
+
+module.exports = mongoose.model('Bid_history', bidhSchema);
